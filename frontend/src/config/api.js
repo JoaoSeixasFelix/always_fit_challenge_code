2806e72ad5898da4344bdc9060332c5adfd86292
JoaoFelix <joaopaulo_seixas@outlook.com>
2024-06-20 21:47:28 -0300

    feat: implementacao dos metodos de conexao

--- frontend/src/config/api.js
+++ frontend/src/config/api.js
@@ -1,7 +1,14 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'http://localhost:8000/', // Exemplo de URL base do backend Laravel
+  baseURL: 'http://localhost:8000', // URL base do seu backend Laravel
+  timeout: 10000, // Tempo limite de 10 segundos (opcional)
+  headers: {
+    'Content-Type': 'application/json',
+    
+  },
+  withCredentials: true, // Configuração para lidar com CORS
+  crossDomain: true, // Configuração para lidar com CORS
 });
 
 export default instance;
