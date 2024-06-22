a466707729bbec5dfee320a64f1a6af835fe5e10
JoaoFelix <joaopaulo_seixas@outlook.com>
2024-06-22 13:59:40 -0300

    feat: Adicionado um interceptador para incluir o token CSRF em todas as requisições

--- frontend/src/config/api.js
+++ frontend/src/config/api.js
@@ -1,12 +1,22 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'http://localhost:8000',
-  timeout: 10000,
+  baseURL: 'http://localhost:8000', // URL base do seu backend Laravel
+  timeout: 10000, // Tempo limite de 10 segundos (opcional)
   headers: {
     'Content-Type': 'application/json',
   },
-  withCredentials: true, 
+  withCredentials: true, // Configuração para lidar com cookies
+});
+
+instance.interceptors.request.use(config => {
+  const token = document.head.querySelector('meta[name="csrf-token"]');
+  if (token) {
+    config.headers['X-CSRF-TOKEN'] = token.content;
+  }
+  return config;
+}, error => {
+  return Promise.reject(error);
 });
 
 export default instance;
